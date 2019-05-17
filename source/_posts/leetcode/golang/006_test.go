f5530d356b65a9916ea0823dcb41a2d27e78c354
faaaar <leeyunfans@gmail.com>
2019-05-17 21:01:54 +0800

    Update

---
+++ source/_posts/leetcode/golang/006_test.go
@@ -0,0 +1,28 @@
+package leetcode
+
+import (
+	"testing"
+)
+
+func TestConvert006(t *testing.T) {
+	type args struct {
+		s       string
+		numRows int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"0", args{"LEETCODEISHIRING", 1}, "LEETCODEISHIRING"},
+		{"1", args{"LEETCODEISHIRING", 3}, "LCIRETOESIIGEDHN"},
+		{"2", args{"LEETCODEISHIRING", 4}, "LDREOEIIECIHNTSG"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Convert006(tt.args.s, tt.args.numRows); got != tt.want {
+				t.Errorf("Convert006() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
